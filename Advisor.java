161c4828ca0e817af2cf43ed6720e481f6e07ed0
Adam <johnsonam23@vcu.edu>
2014-11-14 17:51:17 -0500

    added comments

--- Advisor.java
+++ Advisor.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.*;
 
 class Advisor{
 	JFrame mainFrame;
@@ -8,59 +9,69 @@ class Advisor{
 	JLabel headerLabel;
 	
 	public Advisor(){
-		prepareGUI();
+		prepareGUI(); // setup the GUI
 	} // Advisor constructor
 	
 	public static void main(String[] args){
 		Advisor advisor = new Advisor();
-		advisor.menu();
+		advisor.menu(); // add the buttons
 	} // main
 	
+	// setup the GUI
 	private void prepareGUI(){
-		mainFrame = new JFrame("Advisor");
-		mainFrame.setSize(800, 600);
-		mainFrame.setLayout(new GridLayout(3, 1));
+		mainFrame = new JFrame("Advisor"); // 'Advisor' is what will be displayed at the top of the window
+		mainFrame.setSize(800, 600); // size of the window
+		mainFrame.setLayout(new GridLayout(3, 1)); // 3 rows, 1 column
 		
-		headerLabel = new JLabel("", JLabel.CENTER);
+		headerLabel = new JLabel("", JLabel.CENTER); // starts blank
 		
 		mainPanel = new JPanel();
-		mainPanel.setLayout(new GridLayout(3, 1));
+		mainPanel.setLayout(new GridLayout(3, 1)); // 3 rows, 1 column
 		
+		// make the window close when the X is pressed
 		mainFrame.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e){
 				System.exit(0);
 			}
 		});
 		
-		mainFrame.add(headerLabel);
-		mainFrame.add(mainPanel);
-		mainFrame.setVisible(true);		
+		mainFrame.add(headerLabel); // add headerLabel to the mainFrame
+		mainFrame.add(mainPanel); // add mainPanel to the mainFrame
+		mainFrame.setVisible(true);	// show the mainFrame
 	} // prepareGUI
 	
+	// add the buttons
 	private void menu(){
+		// create the buttons
 		JButton students = new JButton("Students");
 		JButton records = new JButton("Records");
 		JButton submissions = new JButton("Graduation Submissions");
 		
+		// sett the command that will be sent when the button is pressed
 		students.setActionCommand("Students");
 		records.setActionCommand("Records");
 		submissions.setActionCommand("Submissions");
 		
+		// set the object that will listen for the button press
 		students.addActionListener(new ButtonClickListener());
 		records.addActionListener(new ButtonClickListener());
 		submissions.addActionListener(new ButtonClickListener());
 		
+		// add the buttons to mainPanel
 		mainPanel.add(students);
 		mainPanel.add(records);
 		mainPanel.add(submissions);
 		
-		mainFrame.setVisible(true);
+		mainFrame.setVisible(true); // redraw mainFrame
 	} // menu
 	
+	// listen for the buttons being pressed
 	private class ButtonClickListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
+			// find out what command was sent
 			String command = e.getActionCommand();
 			
+			// handle each button differently
 			switch(command){
 				case "Students":
 					headerLabel.setText("Students button pressed");
