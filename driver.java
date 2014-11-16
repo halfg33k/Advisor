6ff847e07fb2e67c1b134ffc83a3b2dc9eadce1c
Adam <johnsonam23@vcu.edu>
2014-11-16 12:24:39 -0500

    created queue that will contain the list of all students

---
+++ driver.java
@@ -0,0 +1,11 @@
+public class driver{
+	public static void main(String[] args){
+		studentList list = new studentList();
+		
+		list.addStudent("Patrick", 24, 1);
+		list.addStudent("Kate", 18, 0);
+		list.addStudent("Daniel", 24, 3);
+		
+		System.out.println("placeholder");
+	} // main
+} // class driver
--- studentList.java
+++ studentList.java
@@ -1,5 +1,55 @@
-import java.util.Scanner;
-
 public class studentList{
+	Node root;
+	Node head;
 	
-} // studentList
+	public void addStudent(String name, int id, int grade){
+		Node node = new Node(name, id, grade);
+		
+		if (root == null){
+			root = node;
+			head = root;
+		}
+		else{
+			head.setNext(node);
+			head = node;
+		}
+	} // addStudent
+	
+	/* Insert a method that will return a list containing all
+	 * nodes in the queue.
+	 */
+} // class studentList
+
+class Node{
+	private String name;
+	private int id;
+	private int grade;
+	private Node next;
+	
+	public Node(){
+		name = "John Doe";
+		id = -1;
+		grade = -1;
+		next = null;
+	} // Node constructor
+	
+	public Node(String name, int id, int grade){
+		this.name = name;
+		this.id = id;
+		this.grade = grade;
+		next = null;
+	} // Node constructor
+	
+	public Node getNext(){
+		return next;
+	} // getNext
+	
+	public void setNext(Node next){
+		this.next = next;
+	} // setNext
+	
+	// return the student's information
+	public String toString(){
+		return "Name: " + name + "\nID: " + id + "\nGrade: " + grade;
+	} // toString
+} // class Node
